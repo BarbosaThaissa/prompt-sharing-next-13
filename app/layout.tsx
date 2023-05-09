f519dfcde16880c59f903a30ccbc98dab9e0f99e
Barbosa Thaíssa <thathamaria03@gmail.com>
2023-05-09 20:24:29 -0300

    feat: add .env no gitignore and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,9 @@ import Provider from "@components/Provider";
 export const metadata = {
   title: "Promptopia App",
   description: "Discover & Share AI Prompts",
+  icons: {
+    icon: '/icon.svg',
+  },
 };
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
