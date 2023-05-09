63e86135ed59d6d382d355ca0a28d2ba5499cbaa
Barbosa Thaíssa <thathamaria03@gmail.com>
2023-05-09 20:44:07 -0300

    feat: add all foldens and files

---
+++ app/create-prompt/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CreatePrompt = () => {
+  return (
+    <div>
+      <h1>CreatePrompt</h1>
+    </div>
+  );
+};
+
+export default CreatePrompt;
---
+++ app/update-prompt/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UpdatePrompt = () => {
+  return (
+    <div>
+      <h1>UpdatePrompt</h1>
+    </div>
+  );
+};
+
+export default UpdatePrompt;
